9a466fda7a835778608dbf10b89f3d8ea125a6c4
Elizabeth Hesterlene Mahood <ehm79@cornell.edu>
2020-10-07 16:03:21 -0400

    now makes one volcano plot per condition, added text on plot

--- 13_volcano_plots.R
+++ 13_volcano_plots.R
@@ -2,13 +2,13 @@
 library(dplyr)
 library(ggplot2)
 library(tidyverse)
+library(grid)
 
-mk_fil <- function(indir){
+mk_fil <- function(indir, odir){
   ##indir is a string, path to directory with Leaf and Root subdirs
   ##RETURNS: list with each tissue-cond pair as a dataframe
   tissues <- c('Leaf', 'Root')
-  
-  filelist <- list()
+
   for (tis in tissues){
     readdir <- paste(indir, tis, 'MSDIAL_out', sep = '/')
     print(readdir)
@@ -28,54 +28,45 @@ mk_fil <- function(indir){
       cond <- strsplit(tcond, '.', fixed = T)[[1]][1]
       thiscond <- rep(cond, nrow(temp))
       temp <- cbind(temp, thistis, thiscond)
-      filelist <- append(filelist, list(temp))
+      ##making additional columns
+      ndat <- temp %>% mutate(logpval = -log10(adj_pval)) %>%
+        mutate(threshold = if_else(nfold_change >= 2 & logpval >= 1.30103 |nfold_change <= -2 & logpval >= 1.30103,"A", "B"))
+      ndat$nfold_change[ndat$nfold_change >= 10] <- 10
+      ##initializing text to put on volcano plot
+      grob <- grobTree(textGrob(paste0("n = ", length(which(ndat$nfold_change >= 2 & ndat$logpval >= 1.30103))), x=0.8,  y= 0.85, hjust=0,
+                                gp=gpar(col="blue", fontsize=15)))
+      grob2 <- grobTree(textGrob(paste0("n = ", length(which(ndat$nfold_change <= -2 & ndat$logpval >= 1.30103))), x=0.1,  y= 0.84, hjust=0,
+                                gp=gpar(col="blue", fontsize=15)))
+      
+      if (tis == 'Leaf') color <- 'darkgreen'
+      if (tis == 'Root') color <- 'saddlebrown'
+      pdf(paste0(odir, '/', cond, tis, 'DAM_volcano_plot.pdf'))
+      print(ggplot(ndat, aes(nfold_change,logpval, colour = interaction(thistis, thiscond, threshold), size = threshold)) +
+        geom_point(alpha = 0.5) + 
+        geom_hline(yintercept = 1.3, linetype = 2, alpha = 0.5, size = 1) + 
+        geom_vline(xintercept = 2, linetype = 2, alpha = 0.5, size = 1) +
+        geom_vline(xintercept = -2, linetype = 2, alpha = 0.5, size = 1) +
+        scale_colour_manual(values = c(color, 'black'), labels = cond) +
+        scale_size_manual(values=c(3,1))+
+        xlab("log2 fold change") + ylab("-log10 q-value")+labs(colour = '')+
+        annotation_custom(grob)+ annotation_custom(grob2)+
+        theme_bw()+guides(size = F, colour = F) + ggtitle(paste(tis, cond, sep = ' ')))
+      dev.off()
     }
   }
-  return(filelist)
 }
 
 argg <- commandArgs(T)
-if (length(argg) != 3){
+if (length(argg) != 2){
   stop('ARGS: 1) Path to input directory (with Leaf/Root subdirs)
-       2) Path to desired output directory
-       3) Experiment: Sym OR Hydro')
+       2) Path to desired output directory')
 }
 
 ##setting arguments
-indir <- argg[1]; odir <- argg[2]; exp <- argg[3]
+indir <- argg[1]; odir <- argg[2]
 
 ##reading all in as list
-filelist <- mk_fil(indir)
+mk_fil(indir, odir)
 
-##coecring to df
-dat_tf <- bind_rows(filelist)
-print(head(dat_tf))
-dat_tf$nfold_change[dat_tf$nfold_change > 15] <- 15
-
-##adding additional cols
-ndat <- dat_tf %>% mutate(logpval = -log10(adj_pval)) %>%
-          mutate(threshold = if_else(nfold_change >= 2 & logpval >= 1.30103 |nfold_change <= -2 & logpval >= 1.30103,"A", "B"))%>%
-          mutate(nlabel = paste(thistis, thiscond, threshold, sep = ' '))
-
-##make experiment-specific variables
-possiblecolors <- c("darkblue", "darkorchid1", "darkolivegreen3", "sandybrown", "darkturquoise",
-                    "deeppink1")
-totallayers <- length(unique(ndat$nlabel))/2
-
-colors <- c(possiblecolors[1:totallayers], rep('black', totallayers))
-print(colors)
-labs <- c(unique(ndat$nlabel)[grepl('A$', unique(ndat$nlabel))], rep('', totallayers))
-
-##finally plotting
-pdf(paste0(odir, '/DAM_volcano_plot.pdf'))
-ggplot(ndat, aes(nfold_change,logpval, colour = interaction(thistis, thiscond, threshold), size = threshold)) +
-    geom_point(alpha = 0.5) + 
-    geom_hline(yintercept = 1.3, linetype = 2, alpha = 0.5, size = 1) + 
-    geom_vline(xintercept = 2, linetype = 2, alpha = 0.5, size = 1) +
-    geom_vline(xintercept = -2, linetype = 2, alpha = 0.5, size = 1) +
-    scale_colour_manual(values = colors, labels = labs) +
-    scale_size_manual(values=c(3,1))+
-    xlab("log2 fold change") + ylab("-log10 q-value")+labs(colour = '')+
-    theme_bw()+guides(size = F)
-
+print('Done with volcano plots!')
 dev.off()
